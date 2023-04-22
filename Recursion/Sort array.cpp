fb179751386156e94826a9a67c2ef49c022d97fa
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2023-04-22 10:28:42 +0530

    Update Sort array.cpp

--- Recursion/Sort array.cpp
+++ Recursion/Sort array.cpp
@@ -1,7 +1,7 @@
 /*
 */
 
-//Solution using recursion
+//Solution using recursion. Time complexity O(N^2).
 
 class Solution{
     public:
@@ -31,6 +31,57 @@ class Solution{
     vector<int> sortArr(vector<int>arr, int n){
     //complete the function here
     sor(arr);
+    return arr;
+    }
+};
+
+// Using mergeSort. O(NlogN)
+
+class Solution{
+    public:
+    void merge(vector<int>&arr, int l, int m, int r)
+    {
+        vector<int>v;
+        int ll=l, rl=m+1;
+        while(ll<=m && rl<=r)
+        {
+            if(arr[ll]<=arr[rl])
+            {
+                v.push_back(arr[ll++]);
+            }
+            else
+            {
+                v.push_back(arr[rl++]);
+            }
+        }
+        while(ll<=m)
+        {
+            v.push_back(arr[ll++]);
+        }
+        while(rl<=r)
+        {
+            v.push_back(arr[rl++]);
+        }
+        for(int i=0; i<v.size(); i++)
+        {
+            arr[l++]=v[i];
+        }
+        return ;
+    }
+    void mergesort(vector<int>&arr, int l, int r)
+    {
+        if(l<r)
+        {
+            int m=l+(r-l)/2;
+            mergesort(arr, l, m);
+            mergesort(arr, m+1, r);
+            merge(arr, l, m, r);
+        }
+        return;
+    }
+    vector<int> sortArr(vector<int>arr, int n){
+    //complete the function here
+    mergesort(arr, 0, n-1);
     return arr;
     }
 };
