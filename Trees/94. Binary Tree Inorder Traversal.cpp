d18e487a336babe77467b3851e59575febe7d94b
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2023-09-23 21:55:07 +0530

    Update 94. Binary Tree Inorder Traversal.cpp

--- Trees/94. Binary Tree Inorder Traversal.cpp
+++ Trees/94. Binary Tree Inorder Traversal.cpp
@@ -71,3 +71,59 @@ public:
         return ans;
     }
 };
+
+// Solution using Moris inorder traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+ // 1st case: if left is null, print current node and go right
+ // 2nd case: before going left, make right most node on left subtree connected to current node, then go left
+ // 3rd case: if thread is already pointed to current node, then remove the thread
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        TreeNode* cur = root;
+        while(cur)
+        {
+            if(cur->left==NULL)
+            {
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }
+            else
+            {
+                TreeNode* prev = cur->left;
+                while(prev->right && prev->right!=cur)
+                {
+                    prev = prev->right;
+                }
+                if(prev->right==NULL)
+                {
+                    prev->right = cur; // the right most node of left subtree is NULL so point it to cur
+                    cur = cur->left;
+                }
+                else
+                {
+                    // This is the 3rd case, here prev->right is connected to cur so we need to insert in ans
+                    // and cur is taken to prev->right i.e the node parent to left subtree. So
+                    // since cur reached to subtree parent so now go towards right i.e. cur=cur->right
+                    prev->right = NULL;
+                    ans.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
